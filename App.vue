7fa7664e79f93bf563ffc33c592ca51085c219ff
GulhayoSayfullayeva <gulhayo.sayfullayeva@gmail.com>
2025-05-09 12:15:13 +0200

    prime-vue features

--- App.vue
+++ App.vue
@@ -1,34 +1,58 @@
 <template>
   <div class="container">
-    <Card v-if="searchMode" style="padding: 2rem; height: 30rem; width: 45rem; overflow: hidden; text-align: center">
+    <Card v-if="searchMode" style="padding: 2rem; height: 30rem; width: 45rem; overflow: hidden; text-align: center;">
       <template #title>Enter Book ID: </template>
       <template #content>
 
-        <InputText style="margin-top: 30% " type="text" v-model="id" />
+        <InputText style="margin-top: 30% " type="text" v-model="id" @click="resetError" />
         <br>
-        <Message style="margin: 1rem; " v-if="showError" severity="error" variant="simple">Book with given ID doesnt
-          exist</Message>
+        <p style="margin: 1rem; color: red" v-if="showError">Book with given ID doesnt exist</p>
       </template>
       <template #footer>
-        <Button @click="handleSearch" style="margin-top: 10% " label="Search" outlined />
+        <Button @click="handleSearch" style="margin-top: 10% " icon="pi pi-search" label="Search" outlined />
 
       </template>
     </Card>
-    <Card v-if="detailsMode" style="padding: 2rem; height: 30rem; width: 45rem; overflow: hidden">
+    <Card v-if="detailsMode" style="padding: 2rem; height: auto; width: 45rem; overflow: hidden">
       <template #header>
-        <Button @click="toggleMode" style="margin: 2rem; float:left" label="Back" />
-    </template>
+        <Button @click="toggleMode" style="margin-top: 2rem; margin-left: 1rem; float:left" label="Back" />
+      </template>
       <template #title>Book Title --- {{ this.resultBooks[0].title }}</template>
       <template #content>
-        <p class="m-0">
-          Book Description --- {{ this.resultBooks[0].body }}
-          <br>
-          Published Year --- {{ this.resultBooks[0].publishYear }}
-        </p>
+        <Panel style="margin-top: 1rem;" header="Book Details" toggleable>
+          <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </p>
+        </Panel>
+        <Panel style="margin-top: 1rem;" header="Book Status" toggleable>
+          <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </p>
+        </Panel>
+        <Panel style="margin-top: 1rem;" header="New Versions" toggleable>
+          <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </p>
+        </Panel>
       </template>
     </Card>
   </div>
-  <router-view />
+
 </template>
 
 <script>
@@ -51,7 +75,6 @@ export default {
       this.resultBooks = this.books.filter(book => {
         return book.id === this.id
       })
-      console.log(this.books)
       if (this.resultBooks.length === 0) {
         this.showError = true
 
@@ -63,8 +86,12 @@ export default {
       }
     },
     toggleMode() {
-      this.searchMode = ! this.searchMode
-      this.detailsMode = ! this.detailsMode
+      this.searchMode = !this.searchMode
+      this.detailsMode = !this.detailsMode
+    },
+    resetError() {
+      this.id = ''
+      this.showError = false
     }
   },
   mounted() {
@@ -72,7 +99,6 @@ export default {
       .then((response) => response.json())
       .then((data) => {
         this.books = data
-        console.log(this.books)
       })
       .catch((error) => console.log(error.message));
   },
@@ -81,10 +107,12 @@ export default {
 
 <style>
 .container {
+  margin: 0px;
+  background: #F5F5F5;
   display: flex;
   justify-content: center;
   align-items: center;
   min-height: 100vh;
-  background: #F5F5F5;
+
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
+import 'primeicons/primeicons.css'
+
 import App from '../App.vue'
 import PrimeVue from 'primevue/config'
 import Button from 'primevue/button'
 import Aura from '@primeuix/themes/aura'
 import Card from 'primevue/card'
 import InputText from 'primevue/inputtext'
-import Message from 'primevue/message';
+import Message from 'primevue/message'
+import Panel from 'primevue/panel'
 import router from './router'
 
 const app = createApp(App)
@@ -21,5 +24,6 @@ app.component('Button', Button)
 app.component('Card', Card)
 app.component('InputText', InputText)
 app.component('Message', Message)
+app.component('Panel', Panel)
 app.use(router)
 app.mount('#app')
